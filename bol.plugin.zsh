e5c3d9c0066a0ac3e87eb85049cf2c95e4c91901
Khurram Raza <ikhurramraza@gmail.com>
2021-03-07 19:45:50 +0100

    Add a quote from Klaus Mikaelson about allure of darkness
    Accompanying Stefan and Caroline in digging something against Silas,
    Klaus reminds Stefan of his fun ways in the 20s. And how darkness
    does not only work on the emotionless but everyone.
    
    Source: The Vampire Diaries S04E17

--- bol.plugin.zsh
+++ bol.plugin.zsh
@@ -2,6 +2,7 @@ if [[ -o interactive ]] && [[ -o login ]]; then
     __bol_quotes=(
         # 🧛🏻 The Originals
         "I am...immortal.\n— Klaus Mikaelson"
+        "Don't underestimate the allure of darkness, Stefan.\nEven the purest hearts are drawn to it.\n— Klaus Mikaelson"
     )
 
     printf '%b\n\n' "${__bol_quotes[RANDOM % $#__bol_quotes + 1]}"
