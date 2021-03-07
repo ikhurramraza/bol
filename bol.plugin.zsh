a5ca17feeb0cb6557ff17a91c69f299efb3cc514
Khurram Raza <ikhurramraza@gmail.com>
2021-03-07 19:14:59 +0100

    Add a quote from Klaus Mikaelson about his immortality
    When Marcel Gerard tells Klaus that anyone who breaks his rules dies.
    Klaus breaks one and let him know that he cannot be killed.
    
    Source: The Originals S01E01

--- bol.plugin.zsh
+++ bol.plugin.zsh
@@ -1,5 +1,7 @@
 if [[ -o interactive ]] && [[ -o login ]]; then
     __bol_quotes=(
+        # 🧛🏻 The Originals
+        "I am...immortal.\n— Klaus Mikaelson"
     )
 
     printf '%b\n\n' "${__bol_quotes[RANDOM % $#__bol_quotes + 1]}"
